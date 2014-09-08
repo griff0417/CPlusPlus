f48bb0ac2205cd48b9344b57fa793762d27f44c9
Jason Griffith <griff0417@gmail.com>
2014-09-08 14:26:17 -0400

    Start

---
+++ Project 1/Project 1/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Sep 8, 2014
+ *      Author: JasonsMac
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class main {
+public:
+	main();
+	virtual ~main();
+};
+
+#endif /* MAIN_H_ */
