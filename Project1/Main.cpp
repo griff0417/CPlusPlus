fc83b97a6e2c1d682dff18d062f6d0b7e3f79faa
Ryan Hochmuth <rhochmuth21@gmail.com>
2014-09-09 18:23:27 -0400

    Repository reset/Initial commit

---
+++ Project1/Main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Sep 9, 2014
+ *      Author: Handirocker21
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello" << endl;
+
+	return 0;
+}
